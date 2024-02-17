df482f8a5ee91126029595a43f4ae03a5dce87c4
ItsAbimaran <139359565+ItsAbimaran@users.noreply.github.com>
2024-02-17 21:08:36 +0530

    Add files via upload

---
+++ romantonumeric.py
@@ -0,0 +1,25 @@
+#project3-roman to numeric
+def roman_to_decimal(roman):
+    rn= {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+    
+    total= 0
+    prev_value = 0
+    
+    for numeral in reversed(roman):
+        value = rn[numeral]
+        
+        if value < prev_value:
+            total-= value
+            
+        else:
+            total+= value
+        
+        prev_value = value
+    
+    return total
+
+
+rn= input("Enter a Roman numeral: ")
+rn=rn.upper()
+total= roman_to_decimal(rn)
+print("The decimal equivalent of",rn, "is", total)
